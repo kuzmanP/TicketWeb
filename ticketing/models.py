289725e2ae561031564a5985a2fd3bbdb386a326
Kuzcodes <georgeghartey2@gmail.com>
2023-02-24 23:59:19 +0000

    chore: Added a Seat and Ticket model with unique id(main)

--- ticketing/models.py
+++ ticketing/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import uuid
 # Create your models here.
 
 #Seat Model
@@ -10,3 +11,12 @@ class Seat(models.Model):
 
     def __str__(self):
         return f"{self.row} - {self.number}"
+    
+class Ticket(models.Model):
+    ticket_id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    purchased_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.seat}"    
