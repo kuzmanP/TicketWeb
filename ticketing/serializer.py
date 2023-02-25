17ea5332d439c23b33d509d2c5a50bd271328658
Kuzcodes <georgeghartey2@gmail.com>
2023-02-25 01:11:23 +0000

    chore: Added a serializer.py(main)

---
+++ ticketing/serializer.py
@@ -0,0 +1,19 @@
+# from rest_framework import serializers
+# from django.contrib.auth.models import User
+# from users.models import Profile
+# class ProfileSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model= Profile
+#         fields=['bio','contact','User_type','user']   
+    
+#     def create(self, validated_data):
+#         profile = Profile.objects.create(
+#             bio=validated_data['bio'],
+#             contact=validated_data['contact'],
+#             User_type=validated_data['User_type'],
+#             user=validated_data['user']
+            
+            
+#         )
+#         profile.save()
+#         return profile
