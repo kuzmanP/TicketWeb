1b6e533c141bcd4a9e334f750e50f7ba742a7700
Kuzcodes <georgeghartey2@gmail.com>
2023-02-25 01:22:29 +0000

    chore: Added a Seat serializer(main)

--- ticketing/serializer.py
+++ ticketing/serializer.py
@@ -1,19 +1,18 @@
-# from rest_framework import serializers
-# from django.contrib.auth.models import User
-# from users.models import Profile
-# class ProfileSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model= Profile
-#         fields=['bio','contact','User_type','user']   
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from ticketing.models import Seat,Ticket
+class SeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Seat
+        fields=['row','available']   
     
-#     def create(self, validated_data):
-#         profile = Profile.objects.create(
-#             bio=validated_data['bio'],
-#             contact=validated_data['contact'],
-#             User_type=validated_data['User_type'],
-#             user=validated_data['user']
+    def create(self, validated_data):
+        seat = Seat.objects.create(
+            row=validated_data['row'],
+            available=validated_data['available'],
+           
             
             
-#         )
-#         profile.save()
-#         return profile
+        )
+        seat.save()
+        return seat
