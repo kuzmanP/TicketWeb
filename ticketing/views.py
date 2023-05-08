aa5deccaf6784d1cfd25639390ce73f9fdd66729
Kuzcodes <georgeghartey2@gmail.com>
2023-05-08 23:56:37 +0000

    chore: added a QRcode generation view in the view.py (ticketing)

--- ticketing/views.py
+++ ticketing/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render,redirect
 from ticketing.models import Seat,Ticket
+import qrcode
+from django.http import HttpResponse
 # Create your views here.
 
 def purchase_ticket(request, seat_id):
@@ -14,3 +16,17 @@ def purchase_ticket(request, seat_id):
     seat.available = False
     seat.save()
     return redirect('ticket_detail', ticket_id=ticket.id)
+
+
+
+def generate_qr_code(request, ticket_id):
+    # Generate the QR code
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(ticket_id)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='black', back_color='white')
+    
+    # Return the image as an HTTP response
+    response = HttpResponse(content_type='image/png')
+    img.save(response, 'PNG')
+    return response
