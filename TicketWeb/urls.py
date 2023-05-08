e2483375a9cbee30ae025fdb9d538bb266cdc374
Kuzcodes <georgeghartey2@gmail.com>
2023-05-08 10:43:25 +0000

    chore: Configured the Url of the app (main)

--- TicketWeb/urls.py
+++ TicketWeb/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('index/', include('ticketing.url')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
+    
 ]
