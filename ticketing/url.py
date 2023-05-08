a16eda172fa5ee735b7297765b1127f1bbda06d7
Kuzcodes <georgeghartey2@gmail.com>
2023-05-08 11:13:31 +0000

    chore: Changed the title of the swagger APi(ticketing)

--- ticketing/url.py
+++ ticketing/url.py
@@ -10,7 +10,7 @@ from drf_yasg.views import get_schema_view as swagger_get_schema_view
 
 schema_view = swagger_get_schema_view(
     openapi.Info(
-        title="FileOcean API",
+        title="TicketWeb API",
         default_version='1.0.0',
         description="API documentation of App",
     ),
