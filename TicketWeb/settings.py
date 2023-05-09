65b2d6acf71fe784420e77a50be3f359a645ff8c
kuzmanP <georgeghartey2@gmail.com>
2023-05-09 15:18:10 +0000

    chore: Resolved(main)

--- TicketWeb/settings.py
+++ TicketWeb/settings.py
@@ -46,10 +46,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_swagger',
     'drf_yasg',
-<<<<<<< HEAD
   
-=======
->>>>>>> origin/main
 ]
 
 MIDDLEWARE = [
--- ticketing/models.py
+++ ticketing/models.py
@@ -68,11 +68,7 @@ TicketTypeCHOICES = [
     ("VV", "VVIP")
 ]
 class Ticket(models.Model):
-<<<<<<< HEAD
-    id= models.UUIDField(primary_key=True, default=uuid.uuid4)
-=======
     id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
->>>>>>> origin/main
     ticket_type=models.CharField(choices=TicketTypeCHOICES,max_length=1000, default="Regular")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
