b6f6689747e9b9ad58360426e5e15cb50b38e1f9
Kuzcodes <georgeghartey2@gmail.com>
2023-05-09 00:48:51 +0000

    chore: Changed the database from sqlite to postgresql(users)

--- TicketWeb/settings.py
+++ TicketWeb/settings.py
@@ -86,11 +86,14 @@ WSGI_APPLICATION = 'TicketWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ticketwebdb',
+        'USER': 'kuzman',
+        'PASSWORD':'rootpass',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
