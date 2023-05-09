97c1d49a985f085274e1c0fb6603511543031757
Kuzcodes <georgeghartey2@gmail.com>
2023-05-09 00:18:31 +0000

    chore: Registered the Event,EventManager amd Transaction to the DjangoAdmin (ticketing)

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from ticketing.models import Seat,Ticket
+from ticketing.models import Seat,Ticket,Event,EventManager,Transaction
 # Register your models here.
 admin.site.register(Seat)
 admin.site.register(Ticket)
+admin.site.register(Event)
+admin.site.register(EventManager)
+admin.site.register(Transaction)
