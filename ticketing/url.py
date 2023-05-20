5aa0a04fbc5bb2e3507ec254c77a4ed89ce00ae1
kuzmanP <georgeghartey2@gmail.com>
2023-05-20 21:35:04 +0000

    chore: added a route to display all the events available to the user(ticketing)

--- ticketing/url.py
+++ ticketing/url.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view as swagger_get_schema_view
-from .views import UserTicketsView
+from .views import UserTicketsView,EventListView
 
 schema_view = swagger_get_schema_view(
     openapi.Info(
@@ -22,5 +22,6 @@ urlpatterns = [
     path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
     path('tickets/<int:ticket_id>/qr_code/', views.generate_qr_code, name='generate_qr_code'),
     path('ticket', UserTicketsView.as_view(), name='all_tickets'),
+    path('events', EventListView.as_view(), name='all_events'),
 ]
 
